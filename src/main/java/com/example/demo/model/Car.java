3280f2338faad3aa8a5e38293fdd8576cdfe88e3
Mathdola <matthiasdola@gmail.com>
2020-02-17 08:58:02 +0000

    Configure MySQL database

--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -2,7 +2,9 @@ package com.example.demo.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +12,11 @@ import lombok.NonNull;
 
 @Entity
 @Data
+@Table(name="car")
 @NoArgsConstructor
 public class Car {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private @NonNull String name;
 	
